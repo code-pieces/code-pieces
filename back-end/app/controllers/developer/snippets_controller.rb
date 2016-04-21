d46085d31e7da295c8a2eedb58dd77c5952e85c7
Tu Hoang <hatu87@gmail.com>
2016-04-21 17:42:55 +0700

    finish add new snippet

--- back-end/app/controllers/developer/snippets_controller.rb
+++ back-end/app/controllers/developer/snippets_controller.rb
@@ -16,8 +16,15 @@ class Developer::SnippetsController < Developer::DeveloperApplicationController
 
   end
 
+  def show
+    # binding.pry
+    @snippet = Snippet.find_by_id(params[:id])
+    # selected_snippet(@snippet)
+    session[:selected_snippet_id] = @snippet.id
+  end
+
   private
   def snippet_params
-    params.require(:snippet).permit(:name, :description, :contents, :created_by, :language, :scope, :tag_trigger)
+    params.require(:snippet).permit(:name, :description, :contents, :created_by, :language_id, :scope, :tag_trigger)
   end
 end
