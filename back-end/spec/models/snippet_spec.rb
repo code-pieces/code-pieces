0afb56c030610c22032377fb004b2bd3e9428983
Minh Dinh <dtminhpt@gmail.com>
2016-04-25 23:01:34 +0700

    rspec test for Snippet model

--- back-end/spec/models/snippet_spec.rb
+++ back-end/spec/models/snippet_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Snippet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate the presence of name" do
+    snippet = Snippet.new
+    expect(snippet).to be_invalid
+    expect(snippet.errors).to be_include(:name)
+  end
 end
